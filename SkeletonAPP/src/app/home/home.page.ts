1170117e1f18d3c35d786c2556473a0d4f9c8a5d
AlexD30 <yer.vasquez@duocuc.cl>
2022-08-26 20:51:38 -0400

    Copia de proyecto

---
+++ SkeletonAPP/src/app/home/home.page.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(
+    private alertController: AlertController,
+    private activatedRoute: ActivatedRoute
+    ) {}
+    
+
+  usuario={
+    id:"",
+    nombreUsuario:"",
+    password:"",
+    nombre:"",
+    apellido:"",
+    nivelEducacion:"",
+    fechaNacimiento:""
+
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      subHeader: 'Usuario',
+      message: 'Su nombre es '+this.usuario.nombre+' '+this.usuario.apellido,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+  ngOnInit() {
+    this.usuario.nombreUsuario=this.activatedRoute.snapshot.paramMap.get("nombreUsuario");
+    console.log(this.usuario.nombreUsuario)
+  }
+
+  limpiar(){
+  this.usuario.nombre=""
+  this.usuario.apellido=""
+  this.usuario.nivelEducacion=""
+  this.usuario.fechaNacimiento=""
+  }
+
+
+
+}
